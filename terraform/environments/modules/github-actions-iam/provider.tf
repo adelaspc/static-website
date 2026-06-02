6de539d48fc45f25c6a4c4f662b09c99c4d630fd
adelaspc <adela.spinciu@yahoo.com>
2026-06-02 16:13:52 +0300

    fix terraform module provider constraints

--- terraform/environments/modules/github-actions-iam/provider.tf
+++ terraform/environments/modules/github-actions-iam/provider.tf
@@ -1,11 +1,15 @@
 terraform {
+  required_version = ">= 1.6, < 2.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
 
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
     }
   }
 }
