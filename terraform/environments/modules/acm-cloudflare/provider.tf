e6bd73389cb5624dd36f8c24a62142f12fe16ca1
adelaspc <158210376+adelaspc@users.noreply.github.com>
2026-06-02 16:13:52 +0300

    fix terraform module provider constraints

--- terraform/environments/modules/acm-cloudflare/provider.tf
+++ terraform/environments/modules/acm-cloudflare/provider.tf
@@ -1,11 +1,15 @@
 terraform {
+  required_version = ">= 1.6, < 2.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
 
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = "~> 5.0"
     }
   }
 }
